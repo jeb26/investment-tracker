2549abb01ec5340dc81b468111ccc127b99ad498
James Edward Bell <bell.edward.james@gmail.com>
2013-11-14 02:25:18 -0500

    scrape.sh function complete

--- scrape.sh
+++ scrape.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+#counters and constants
+TIME_TO_SLEEP=5
+TIMES_TO_SCRAPE=2
+counter=0
+
 #website to scrape
 URL="http://markets.usatoday.com/custom/usatoday-com/html-mktscreener.asp"
 
-#tokens for the filename
-year=`date "+%Y_%m_%d_%l_%M_%S" | cut -d'_' -f1`
-month=`date "+%Y_%m_%d_%l_%M_%S" | cut -d'_' -f2`
-day=`date "+%Y_%m_%d_%l_%M_%S" | cut -d'_' -f3`
-hour=`date "+%Y_%m_%d_%l_%M_%S" | cut -d'_' -f4`
-minutes=`date "+%Y_%m_%d_%l_%M_%S" | cut -d'_' -f5`
-secondss=`date "+%Y_%m_%d_%l_%M_%S" | cut -d'_' -f6`
+#date command to be run multiple times
+GET_DATE="date "+%Y_%m_%d_%l_%M_%S""
+GET_FIELD="cut -d'_' "
 
 #echo "year:$year month:$month day:$day hour:$hour minutes:$minutes seconds:$secs"
 
-#counters and constants
-MINUTE=60
-counter=0
-while [ $counter -lt 2 ]
+while [ ${counter} -lt ${TIMES_TO_SCRAPE} ]
 do
-	wget -O usatoday_${year}__${month}_${day}_${hour}_${minutes}_${seconds}.html $URL
-	sleep $MINUTE
+	#time tokens
+	year=`date "+%Y_%m_%e_%I_%M_%S" | cut -d'_' -f1`
+	month=`date "+%Y_%m_%e_%I_%M_%S" | cut -d'_' -f2`
+	day=`date "+%Y_%m_%e_%I_%M_%S" | cut -d'_' -f3`
+	hour=`date "+%Y_%m_%e_%I_%M_%S" | cut -d'_' -f4`
+	minutes=`date "+%Y_%m_%e_%I_%M_%S" | cut -d'_' -f5`
+	secs=`date "+%Y_%m_%e_%I_%M_%S" | cut -d'_' -f6`
+	
+	#begin webpage download with filename containing time tokens
+	wget -O pages/usatoday_${year}_${month}_${day}_${hour}_${minutes}_${secs}.html $URL
+	#echo ${day}${hour}
+	#echo "${year}_${month}_${day}_${hour}_${minutes}_${secs}.html"
+
+	sleep ${TIME_TO_SLEEP}
 	counter=$(($counter + 1))
 done
